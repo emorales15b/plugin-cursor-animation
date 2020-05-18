cc84973cd54ef4d43800d4635684230f82ff7e5e
Morales <emorales15@gmail.com>
2020-05-18 18:02:54 -0500

    Segundo cambio

--- admin/includes/dashboard.php
+++ admin/includes/dashboard.php
@@ -2,7 +2,7 @@
 wp_enqueue_script( 'functions_dashboard', plugin_dir_url( __FILE__ ) . 'js/functions.js', array ( 'jquery' ), 1.1, true);
 ?>
 
-<div>Dashboard</div>
+<div>Efectos</div>
     <select id="efectos">
     <option value="estrellas">Estrellas</option>
     <option value="burbujas">Burbujas</option>
