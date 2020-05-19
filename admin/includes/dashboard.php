350eba0368bef6dd2554a8353b8ac6492f4f676a
Morales <emorales15@gmail.com>
2020-05-19 17:58:19 -0500

    Tercer cambio

--- admin/includes/dashboard.php
+++ admin/includes/dashboard.php
@@ -1,7 +1,7 @@
 <?php
 wp_enqueue_script( 'functions_dashboard', plugin_dir_url( __FILE__ ) . 'js/functions.js', array ( 'jquery' ), 1.1, true);
 ?>
-
+<div>Erick Giovanny Morales Bustamante<div>
 <div>Efectos</div>
     <select id="efectos">
     <option value="estrellas">Estrellas</option>
